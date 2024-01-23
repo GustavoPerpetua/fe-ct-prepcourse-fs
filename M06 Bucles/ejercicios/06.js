ac1ca57720ed2ddacb51c5e21fb90685dfc98958
Gustavo Perpetua <gustavoaperpe@gmail.com>
2024-01-22 22:29:42 -0300

    agregando ejercicios modulo 6

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,6 +2,7 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  const numStr = num.toString();
+  return numStr.length === 3;
 }
-
 module.exports = tieneTresDigitos;
