2966ab84ee5dc29af32d9551711f22ce3571b11f
Gustavo Perpetua <gustavoaperpe@gmail.com>
2024-01-31 16:55:29 -0300

    lala

--- M07 Arrays/ejercicios/02.js
+++ M07 Arrays/ejercicios/02.js
@@ -2,6 +2,11 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  return array.sort(function(a, b) {
+    return a - b;
+  });
 }
-
+const miArreglo = [5, 2, 9, 1, 7];
+const arregloOrdenado = ordenarArray(miArreglo);
+console.log(arregloOrdenado);
 module.exports = ordenarArray;
